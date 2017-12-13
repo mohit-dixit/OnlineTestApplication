287bceea87c99c8d31d60cdbf8c8d91fee5e1f87
Nitish Narang <nitishnarang2007@gmail.com>
2017-12-13 15:35:28 +0530

    Create teacher & Config Module

--- src/components/AdminConfiguration/AdminConfiguration.component.js
+++ src/components/AdminConfiguration/AdminConfiguration.component.js
@@ -1,63 +1,103 @@
 import SuccessNotification from '../SuccessNotification'
+import * as config from '../../config/constants.js'
 import Vue from 'vue'
 import {
-  GetRequest,
-  PostRequest,
-  NumberKeyValidation
+    GetRequest,
+    PostRequest,
+    NumberKeyValidation
 } from '../../utils/globalservice'
 import Multiselect from 'vue-multiselect'
 
-export default  {
-  name: 'admin-configuration',
-  components: {
-    'success-notification': SuccessNotification,
-    Multiselect
-  },
-  props: [],
-  data () {
-    return {
-      adminconfigurationform: {},
-      value: [{
-        text: 'Easy',
-        value: 1
-      },
-      {
-        text: 'Medium',
-        value: 2
-      },
-      {
-        text: 'Difficult',
-        value: 3
-      }
-    ],
-    scaleOptions: [{
-        text: 'Easy',
-        value: 1
-      },
-      {
-        text: 'Medium',
-        value: 2
-      },
-      {
-        text: 'Difficult',
-        value: 3
-      }
-    ]
+export default {
+    name: 'admin-configuration',
+    components: {
+        'success-notification': SuccessNotification,
+        Multiselect
+    },
+    props: [],
+    data() {
+        this.BaseUrl = config.BASE_URL;
+        return {
+            adminconfigurationform: {
+                allow_scale: true
+            },
+            value: [{
+                text: 'Easy',
+                value: 1
+            }, {
+                text: 'Medium',
+                value: 2
+            }, {
+                text: 'Difficult',
+                value: 3
+            }],
+            scaleOptions: [{
+                text: 'Easy',
+                value: 1
+            }, {
+                text: 'Medium',
+                value: 2
+            }, {
+                text: 'Difficult',
+                value: 3
+            }]
+        }
+    },
+    computed: {
+
+    },
+    mounted() {
+
+    },
+    methods: {
+        onSubmit() {
+            alert(JSON.stringify(this.adminconfigurationform));
+            let apiPath = 'api/admin/update/config';
+            // let isEditMode = this.isEdit;
+            // if (isEditMode) {
+            //     apiPath = 'api/admin/update/teacher';
+            // }
+            
+            this.adminconfigurationform.allow_scale = this.adminconfigurationform.allow_scale  ? 1 : 2;
+            this.adminconfigurationform.allow_subject = this.adminconfigurationform.allow_subject ? 1 : 2;
+            this.adminconfigurationform.allow_student = this.adminconfigurationform.allow_student ? 1 : 2;
+            
+            PostRequest(this.BaseUrl + apiPath, this.adminconfigurationform).then(res => {
+                if (res) {
+                    if (res.status == 200) {
+                        // this.createteacherform = {};
+                        // if (isEditMode) {
+                        //     alert('Configuration Changed Successfully')
+                        //     this.$router.push('/Dashboard/TeacherList');
+                        // } else {
+                        //     this.notifySuccess = true;
+                        //     this.notifyError = false;
+                        //     this.responseMessage = 'Teacher created successfully';
+                        //     this.$forceUpdate();
+                        // }
+                    } 
+                    // else {
+                    //     this.errorMessage = res.statustext;
+                    //     this.notifySuccess = false;
+                    //     this.notifyError = true;
+                    //     this.$forceUpdate();
+                    // }
+                }
+            });
+        }
+
+    },
+    created: function() {
+        this.loginRole = Vue.lsobj.get('loginRole');
+        this.instituteID = Vue.lsobj.get('instituteID');
+        this.allow_scale = Vue.lsobj.get('allow_scale');
+        this.allow_student = Vue.lsobj.get('allow_student');
+        this.allow_subject = Vue.lsobj.get('allow_subject');
+        debugger;
+        this.adminconfigurationform.allow_scale = this.allow_scale == 1 ? true : false;
+        this.adminconfigurationform.allow_subject = this.allow_subject == 1 ? true : false;
+        //this.adminconfigurationform.allow_batch = true;
+        this.adminconfigurationform.allow_student = this.allow_student == 1 ? true : false;
+        debugger;
     }
-  },
-  computed: {
-
-  },
-  mounted () {
-
-  },
-  methods: {
-    onSubmit() {
-      alert(JSON.stringify(this.adminconfigurationform));
-    }
-
-  },
-  created: function () {
-    this.loginRole = Vue.lsobj.get('loginRole');
-  }
 }
--- src/components/TeacherList/TeacherList.component.js
+++ src/components/TeacherList/TeacherList.component.js
@@ -1,120 +1,123 @@
 import Vue from "vue";
 import {
-  GetRequest,
-  PostRequest
+    GetRequest,
+    PostRequest
 } from '../../utils/globalservice'
 import * as config from '../../config/constants.js'
 export default {
-  name: 'teacher-list',
-  components: {},
-  props: [],
-  data() {
-    this.BaseUrl = config.BASE_URL;
-    this.teacherList = [];
-    this.selectedId = 0;
-    return {
-      variants: [
-        'primary', 'secondary', 'success', 'warning', 'danger', 'info', 'light', 'dark'
-      ],
-      headerBgVariant: 'dark',
-      headerTextVariant: 'light',
-      bodyBgVariant: 'light',
-      bodyTextVariant: 'dark',
-      footerBgVariant: 'warning',
-      footerTextVariant: 'dark',
-      columnsTeachers: [{
-          label: 'Id',
-          field: 'id',
-          visible: false
-        },
-        {
-          label: 'First Name',
-          field: 'firstname',
-          filterable: true
-        },
-        {
-          label: 'Last Name',
-          field: 'lastname',
-          filterable: true
-        },
-        {
-          label: 'Phone',
-          field: 'phone',
-          filterable: true
-        },
-        {
-          label: 'Email/Username',
-          field: 'email',
-          filterable: true
-        },
-        {
-          label: 'Action'
+    name: 'teacher-list',
+    components: {},
+    props: [],
+    data() {
+        this.BaseUrl = config.BASE_URL;
+        //this.teacherList = [];
+        this.selectedId = 0;
+        return {
+            variants: [
+                'primary', 'secondary', 'success', 'warning', 'danger', 'info', 'light', 'dark'
+            ],
+            teacherList: [],
+            headerBgVariant: 'dark',
+            headerTextVariant: 'light',
+            bodyBgVariant: 'light',
+            bodyTextVariant: 'dark',
+            footerBgVariant: 'warning',
+            footerTextVariant: 'dark',
+            columnsTeachers: [
+                // {
+                //   label: 'Id',
+                //   field: 'id',
+                //   visible: false
+                // },
+                {
+                    label: 'First Name',
+                    field: 'firstname',
+                    filterable: true
+                }, {
+                    label: 'Last Name',
+                    field: 'lastname',
+                    filterable: true
+                }, {
+                    label: 'Phone',
+                    field: 'phone',
+                    filterable: true
+                }, {
+                    label: 'Email/Username',
+                    field: 'email',
+                    filterable: true
+                }, {
+                    label: 'Status',
+                    field: 'status',
+                    filterable: true
+                }, {
+                    label: 'Action'
+                }
+            ]
         }
-      ]
-    }
-  },
-  computed: {},
-  mounted() {
+    },
+    computed: {},
+    mounted() {
 
-  },
-  methods: {
-    bindTeachers: function () {
-      GetRequest(this.BaseUrl + 'api/admin/teacher/list').then(res => {
-        if (res.status) {
-          let response = res.result.message[0].user_roles;
-          let list = [];
-          response.forEach(function (element) {
-            let userObject = element.user;
-            list.push({
-              id: userObject.id,
-              firstname: userObject.firstname,
-              lastname: userObject.lastname,
-              phone: userObject.phone,
-              email: userObject.username
-            })
-          }, this);
-          this.teacherList = list;
-          this.$forceUpdate();
-        } else {
-          this.teacherList = [];
-        }
-      });
     },
-    redirectToNewTeacher: function () {
-      this.$router.push('/Dashboard/CreateTeacher');
-    },
-    editTeacherClick: function (events, args) {
-      this.$router.push({
-        name: 'EditTeacher',
-        params: {
-          id: events.row.id
-        }
-      });
-    },
-    deleteTeacherClick: function (events, args) {
-      this.selectedId = events.row.id;
-      this.$refs.deleteModal.show();
-    },
-    closeModal: function (events, args) {
-      this.$refs.deleteModal.hide();
-    },
-    deleteConfirmation: function () {
-      if (this.selectedId) {
-        let postData = {};
-        postData.id = this.selectedId;
-        PostRequest(this.BaseUrl + 'api/admin/delete/user', postData).then(res => {
-          if (res) {
-            if (res.status == 200) {
-              this.bindTeachers();
-              this.$refs.deleteModal.hide();
+    methods: {
+        bindTeachers: function(params) {
+            GetRequest(this.BaseUrl + 'api/admin/teacher/list').then(res => {
+                if (res.status) {
+                    let response = res.result.message[0].user_roles;
+                    let list = [];
+                    res.result.message.forEach(function(element) {
+                        list.push({
+                            id: element.id,
+                            firstname: element.firstname,
+                            lastname: element.lastname,
+                            phone: element.phone,
+                            email: element.username,
+                            status: element.status == 1 ? 'Active' : 'Inactive'
+                        })
+                    }, this);
+                    this.teacherList = list;
+                    this.$forceUpdate();
+
+                } else {
+                    this.teacherList = [];
+                }
+            });
+        },
+        redirectToNewTeacher: function() {
+            this.$router.push('/Dashboard/CreateTeacher');
+        },
+        editTeacherClick: function(events, args) {
+            this.$router.push({
+                name: 'EditTeacher',
+                params: {
+                    id: events.row.id
+                }
+            });
+        },
+        deleteTeacherClick: function(events, args) {
+            this.selectedId = events.row.id;
+            this.$refs.deleteModal.show();
+        },
+        closeModal: function(events, args) {
+            this.$refs.deleteModal.hide();
+        },
+        deleteConfirmation: function() {
+            if (this.selectedId) {
+                let postData = {};
+                postData.id = this.selectedId;
+                PostRequest(this.BaseUrl + 'api/admin/delete/user', postData).then(res => {
+                    if (res) {
+                        if (res.status == 200) {
+                            this.bindTeachers("delete");
+                            this.$refs.deleteModal.hide();
+                        }
+                    }
+                });
             }
-          }
-        });
-      }
+        }
+    },
+    created: function() {
+        this.loginRole = Vue.lsobj.get('loginRole');
+        this.bindTeachers();
     }
-  },
-  created: function () {
-    this.loginRole = Vue.lsobj.get('loginRole');
-    this.bindTeachers();
-  }
 }
