7540ee8b8e07cd16de672fb2a3d55b5247ed5c5a
SupriyaSgn <supriyasingh.ggit@gmail.com>
2017-12-18 08:31:41 +0530

    Changes in Create Test

---
+++ src/components/SelectQuestionsView/index.vue
@@ -0,0 +1,4 @@
+<template src="./SelectQuestionsView.component.html"></template>
+<script src="./SelectQuestionsView.component.js"></script>
+<style src="./SelectQuestionsView.component.css" scoped></style>
+
